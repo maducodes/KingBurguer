e615c8a6d9b3d24538c970e24823ba1073ee7313
Maria Eduarda Ferreira Alves <marih.silva@icloud.com>
2026-09-05 18:57:24 -0300

    configure initial sceneDelegate

--- KingBurguer/ViewController.swift
+++ KingBurguer/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  KingBurguer
-//
-//  Created by Maria Eduarda Ferreira Alves on 05/09/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
