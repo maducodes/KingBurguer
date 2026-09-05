143301e3ff3edebbf86a1d2c8a29fd5d1c842c56
Maria Eduarda Ferreira Alves <mariaeduardaalves@MacBook-Air-de-Maria.local>
2026-09-05 14:41:36 -0300

    initial commit

---
+++ KingBurguer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurguer
+//
+//  Created by Maria Eduarda Ferreira Alves on 05/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
